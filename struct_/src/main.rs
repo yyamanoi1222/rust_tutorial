9542a3c2e65e242b922f86d9b8b12b08893d8814
Yuu yamanoi <yuu.yamanoi1222@gmail.com>
2023-01-18 23:51:51 +0900

    .5

--- struct_/src/main.rs
+++ struct_/src/main.rs
@@ -21,6 +21,12 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
