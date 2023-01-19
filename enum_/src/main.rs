9b6d62df618310dd3811df343c1a11093485ac60
Yuu yamanoi <yuu.yamanoi1222@gmail.com>
2023-01-19 20:43:25 +0900

    .6

---
+++ enum_/src/main.rs
@@ -0,0 +1,34 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+}
+
+impl Message {
+    fn call(&self) {
+    }
+}
+
+
+fn route(ip_type: IpAddrKind) {}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let four = IpAddrKind::V4(127, 0, 0, 1);
+    let six = IpAddrKind::V6(String::from("::1"));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
