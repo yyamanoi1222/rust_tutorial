7c60f34f3194b2efe40b2065159d466c6e0f8ba1
Yuu Yamanoi <yuu.yamanoi1222@gmail.com>
2023-01-23 23:50:47 +0900

    7.5

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -24,28 +24,16 @@ mod back_of_house {
     fn cook_order() {}
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+use crate::front_of_house::hosting;
 
 pub fn eeat_at_restaurant() {
     // abs
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // rel
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("test");
     meal.toast = String::from("a");
